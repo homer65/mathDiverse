6b0d7e73bb1318f93218bb042abbcf5ee907d06f
Christian Ehm <christian@myoggradio.org>
2020-07-31 11:41:56 +0200

    Visualizer kann jetzt auch zwei Funktionen

--- m/Visualizer.java
+++ m/Visualizer.java
@@ -6,4 +6,9 @@ public interface Visualizer
 	 * anzahl ist die Anzahl der zu berechnenden Funktionswerte
 	 */
 	void show(Funktion f,double x1,double x2,int anzahl);
+	/*
+	 * Zeigt die Funktion f und g im Intervall [x1,x2] grafisch an.
+	 * anzahl ist die Anzahl der zu berechnenden Funktionswerte
+	 */
+	void show(Funktion f,Funktion g,double x1,double x2,int anzahl);
 }
--- m/impl/JFreeChartVisualizer.java
+++ m/impl/JFreeChartVisualizer.java
@@ -39,4 +39,37 @@ public class JFreeChartVisualizer implements Visualizer
 	    frame.pack();
 	    frame.setVisible(true);
 	}
+	@Override
+	public void show(Funktion f,Funktion g,double x1, double x2, int anzahl) 
+	{
+		XYSeries series1 = new XYSeries(f.getName());
+		XYSeries series2 = new XYSeries(g.getName());
+		double d = (double) anzahl;
+		double dx = (x2 - x1) / d;
+		double x = x1;
+		for (int i=0;i<anzahl;i++)
+		{
+			double y1 = f.get(x);
+			double y2 = g.get(x);
+			series1.add(x,y1);
+			series2.add(x,y2);
+			x += dx;
+		}
+		XYSeriesCollection collection = new XYSeriesCollection();
+		collection.addSeries(series1);
+		collection.addSeries(series2);
+		JFreeChart xylineChart = ChartFactory.createXYLineChart(
+		         "",
+		         "" ,
+		         "" ,
+		         collection,
+		         PlotOrientation.VERTICAL ,
+		         true , true , false);
+		ChartPanel chartPanel = new ChartPanel(xylineChart );
+	    chartPanel.setPreferredSize( new java.awt.Dimension(1120,734));
+	    JFrame frame = new JFrame();
+	    frame.setContentPane(chartPanel);
+	    frame.pack();
+	    frame.setVisible(true);
+	}
 }
